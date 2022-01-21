9c80b5082dceec7b8791e9a39bfdadfc813ec87b
Loïc Mangeonjean <loic@codingame.com>
2022-01-21 12:41:40 +0100

    Rename module

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,8 +4,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/lighttiger2505/sqls/ast"
-	"github.com/lighttiger2505/sqls/token"
+	"github.com/CodinGame/sqls/ast"
+	"github.com/CodinGame/sqls/token"
 )
 
 func TestParseStatement(t *testing.T) {
