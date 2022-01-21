a78b6ba899de26b1dcaff5cf0b9ce9c8ae6417bb
Loïc Mangeonjean <loic@codingame.com>
2022-01-21 12:41:40 +0100

    Rename module

--- internal/handler/completion.go
+++ internal/handler/completion.go
@@ -5,8 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/lighttiger2505/sqls/internal/completer"
-	"github.com/lighttiger2505/sqls/internal/lsp"
+	"github.com/CodinGame/sqls/internal/completer"
+	"github.com/CodinGame/sqls/internal/lsp"
 	"github.com/sourcegraph/jsonrpc2"
 )
 
